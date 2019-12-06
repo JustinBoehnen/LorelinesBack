31d315f44d215d546e3531057da77a7a5b926c82
JustinBoehnen <justin.boehnen@oit.edu>
2019-12-06 15:44:26 -0800

    Added password encryption to login and registration

--- routes/users.js
+++ routes/users.js
@@ -3,30 +3,37 @@ const router = express.Router()
 const status = require('http-status-codes')
 const User = require('../models/User.model')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 // These routes are in: api/user
 // TODO: compare passwords to encrypted passwords
 
 /**
  * Purpose: Adds a new user to the DB
- * Full path: /api/user/
- * req: email and encrypted password
+ * Full path: /api/users/
+ * req: email and plaintext password
  * res: token
  */
 router.post('/', (req, res) => {
-  var user = new User({
-    id: req.body.id,
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  })
+  bcrypt.hash(req.body.password, process.env.SALT_ROUNDS, (err, hash) => {
+    if (!err) {
+      var user = new User({
+        id: req.body.id,
+        name: req.body.name,
+        email: req.body.email,
+        password: hash
+      })
 
-  user.save((err, doc) => {
-    if (!err) res.send(User.generateJwt(user))
-    else {
-      res
-        .status(status.CONFLICT)
-        .send(['ERR: user with that email already exists'])
+      user.save((err, doc) => {
+        if (!err) res.send(User.generateJwt(user))
+        else {
+          res
+            .status(status.CONFLICT)
+            .send(['ERR: user with that email already exists'])
+          console.log(err)
+        }
+      })
+    } else {
       console.log(err)
     }
   })
@@ -34,30 +41,28 @@ router.post('/', (req, res) => {
 
 /**
  * Purpose: Logs a user into the site
- * Full path: /api/user/token
- * req: email and encrypted password
+ * Full path: /api/users/token
+ * req: email and plaintext password
  * res: token
  */
 router.post('/token', (req, res) => {
-  User.findOne(
-    { email: req.body.email, password: req.body.password },
-    (err, user) => {
-      if (!err && user !== null) {
-        if (user.password === req.body.password) {
-          res.status(status.OK).send(User.generateJwt(user))
-        }
-      } else {
-        res.sendStatus(status.NOT_FOUND)
-      }
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!err && user !== null) {
+      bcrypt.compare(req.body.password, user.password, (err, res) => {
+        if (res) res.status(status.OK).send(User.generateJwt(user))
+        else res.sendStatus(status.UNAUTHORIZED)
+      })
+    } else {
+      res.sendStatus(status.NOT_FOUND)
     }
-  )
+  })
 })
 
 /**
  * Purpose: Updates the users token with a
  *          newer token if they enter the site
  *          before the old one expires
- * Full path: /api/user/token
+ * Full path: /api/users/token
  * req: token
  * res: new token
  */
