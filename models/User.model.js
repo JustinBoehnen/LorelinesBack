be589735f32da9248fee6bea763251b91b8722ff
JustinBoehnen <justin.boehnen@oit.edu>
2019-12-06 15:56:18 -0800

    Added password encryption to login and registration

--- models/User.model.js
+++ models/User.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
+//const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 //Defaults for user accounts
@@ -26,27 +26,27 @@ var userSchema = new mongoose.Schema({
   saltSecret: String
 })
 
-// Custom validation for email
-userSchema.path('email').validate(val => {
-  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  return emailRegex.test(val)
-}, 'Invalid Email.')
+// // Custom validation for email
+// userSchema.path('email').validate(val => {
+//   emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//   return emailRegex.test(val)
+// }, 'Invalid Email.')
 
-//Encrypt/Salt Password before save
-userSchema.pre('save', next => {
-  bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(this.Password, salt, (err, hash) => {
-      this.Password = hash
-      this.saltSecret = salt
-      next()
-    })
-  })
-})
+// //Encrypt/Salt Password before save
+// userSchema.pre('save', next => {
+//   bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(this.Password, salt, (err, hash) => {
+//       this.Password = hash
+//       this.saltSecret = salt
+//       next()
+//     })
+//   })
+// })
 
-//comparing encrpted and plain text password
-userSchema.methods.verifyPassword = Password => {
-  return bcrypt.compareSync(Password, this.Password)
-}
+// //comparing encrpted and plain text password
+// userSchema.methods.verifyPassword = Password => {
+//   return bcrypt.compareSync(Password, this.Password)
+// }
 
 userSchema.statics.generateJwt = user => {
   return jwt.sign(
--- mongooseClient.js
+++ mongooseClient.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
 
+console.log('URI:', process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
-mongoose.connection.on('connected', () => console.log('DB connected'))
+mongoose.connection.on('connected', () =>
+  console.log('MongoDB Database connected!')
+)
 mongoose.connection.on('error', err => console.log(err))
