9739be77c026d9c6907c0522e1189dc166412eec
Evan-D-Clark <evan.clark@oit.edu>
2020-01-21 13:56:02 -0800

    Added comments

--- routes/personEntity.js
+++ routes/personEntity.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const status = require("http-status-codes");
 const User = require("../models/personEntity.model");
 const jwt = require("jsonwebtoken");
-
+/**
+ * Purpose: Adds a new person entity instance
+ * Full path: /api/personEntity/addPersonEntity
+ * req: personEntity fields
+ * res: token
+ */
 app.post("/addPersonEntity", (req, res) => {
   var myData = new personEntity(req.body);
   myData
