ad8e3d6900f69bbe955f26821c52c32c5cac029b
Evan-D-Clark <evan.clark@oit.edu>
2020-04-23 13:50:21 -0700

    User model tests

--- models/__tests__/user.model.test.js
+++ models/__tests__/user.model.test.js
@@ -1,4 +1,3 @@
-const mongoose = require("mongoose");
 const dbHandler = require("../../db-handler");
 const userModel = require("../user.model");
 
@@ -17,5 +16,56 @@ afterAll(async () => {
 describe("user model test", () => {
   it("has a module", () => {
     expect(userModel).toBeDefined();
+  });
+});
+
+describe("save user", () => {
+  it("saves a user", async () => {
+    const user = new userModel({
+      name: "testUser1",
+      email: "testUser1@email.com",
+      password: "testUser1password",
+      created: new Date(0),
+    });
+    const savedUser = await user.save();
+    const expected = "testUser1";
+    const actual = savedUser.name;
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("get user", () => {
+  it("gets a user", async () => {
+    const user = new userModel({
+      name: "testUser0",
+      email: "testUser0@email.com",
+      password: "testUser0password",
+      created: new Date(0),
+    });
+    await user.save();
+
+    const foundUser = await userModel.findOne({ name: "testUser0" });
+    const expected = "testUser0";
+    const actual = foundUser.name;
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("update a user", () => {
+  it("updates a user", async () => {
+    const user = new userModel({
+      name: "testUser2",
+      email: "testUser2@email.com",
+      password: "testUser2password",
+      created: new Date(0),
+    });
+    await user.save();
+
+    user.name = "testUser2edit";
+    const updatedUser = await user.save();
+
+    const expected = "testUser2edit";
+    const actual = updatedUser.name;
+    expect(actual).toEqual(expected);
   });
 });
