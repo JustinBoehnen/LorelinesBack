8ee2147b8e6bb2d470c85f32c756cd777a9cb3e2
Evan-D-Clark <evan.clark@oit.edu>
2020-05-27 14:11:15 -0700

    Field Content schema bug
    Field content schema for custom entities did not include NUMBER_FIELD as an option

--- models/entityInstance.model.js
+++ models/entityInstance.model.js
@@ -1,27 +1,28 @@
 /** @format */
 
-const mongoose = require('mongoose')
-const User = require('./user.model')
+const mongoose = require("mongoose");
+const User = require("./user.model");
 
 var FieldContent = new mongoose.Schema({
-	type: {
-		type: String,
-		required: [true, 'field type is required'],
-		enum: [
-			'TEXT_FIELD',
-			'LIST_FIELD',
-			'REFERENCE_FIELD',
-			'CHECKBOX_FIELD',
-			'RADIOLIST_FIELD',
-			'IMAGE_FIELD',
-			'SECTION_HEADER',
-			'SECTION_DIVIDER',
-		],
-	},
-	name: { type: String, required: [true, 'field name is required'] },
-	content: [], // used for RADIOLIST and LIST
-	_id: false,
-})
+  type: {
+    type: String,
+    required: [true, "field type is required"],
+    enum: [
+      "TEXT_FIELD",
+      "LIST_FIELD",
+      "NUMBER_FIELD",
+      "REFERENCE_FIELD",
+      "CHECKBOX_FIELD",
+      "RADIOLIST_FIELD",
+      "IMAGE_FIELD",
+      "SECTION_HEADER",
+      "SECTION_DIVIDER",
+    ],
+  },
+  name: { type: String, required: [true, "field name is required"] },
+  content: [], // used for RADIOLIST and LIST
+  _id: false,
+});
 
 // TEXT_FIELD       : A BLOCK OF TEXT (EX: BIO, NAME)
 // LIST_FIELD       : A LIST OF OTHER INSTANCES (EX: FRIENDS)
@@ -33,17 +34,24 @@ var FieldContent = new mongoose.Schema({
 // SECTION_DIVIDER  : A LINE REPRESENTING A SECTION BREAK
 
 var EntityInstanceSchema = new mongoose.Schema({
-	name: { type: String, required: [true, 'instance name is required'] },
-	content: {
-		type: [FieldContent],
-		required: [true, 'instance content is required'],
-	},
-	ownerId: { type: mongoose.Types.ObjectId, ref: 'User', required: [true, 'owner id is required'] },
-})
+  name: { type: String, required: [true, "instance name is required"] },
+  content: {
+    type: [FieldContent],
+    required: [true, "instance content is required"],
+  },
+  ownerId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: [true, "owner id is required"],
+  },
+});
 
-EntityInstanceSchema.pre('remove', { document: true }, (next) => {
-	User.updateOne({ _id: this.ownerId }, { $inc: { 'limits.instances.current': -1 } }) // remove one instance
-	next()
-})
+EntityInstanceSchema.pre("remove", { document: true }, (next) => {
+  User.updateOne(
+    { _id: this.ownerId },
+    { $inc: { "limits.instances.current": -1 } }
+  ); // remove one instance
+  next();
+});
 
-module.exports = mongoose.model('EntityInstance', EntityInstanceSchema)
+module.exports = mongoose.model("EntityInstance", EntityInstanceSchema);
